8765ad87e58e2014ef22f9198544d8870efe0396
wupei2 <wp_zj1@126.com>
2024-02-05 20:40:47 +0800

    fix readrandomwriterandom not do batch

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -7211,6 +7211,10 @@ class Benchmark {
     }
 
     // the number of iterations is the larger of read_ or write_
+    int batch_size = 0;
+    WriteBatch batch(/*reserved_bytes=*/0, /*max_bytes=*/0,
+    FLAGS_write_batch_protection_bytes_per_key, user_timestamp_size_);
+
     while (!duration.Done(1)) {
       DB* db = SelectDB(thread);
       GenerateKeyFromInt(thread->rand.Next() % FLAGS_num, FLAGS_num, &key);
@@ -7242,15 +7246,28 @@ class Benchmark {
         // then do all the corresponding number of puts
         // for all the gets we have done earlier
         Status s;
-        if (user_timestamp_size_ > 0) {
-          Slice ts = mock_app_clock_->Allocate(ts_guard.get());
-          s = db->Put(write_options_, key, ts, gen.Generate());
+
+        if (batch_size > 1) {
+          // 凑batch
+          if ((batch_size + 1) % entries_per_batch_ != 0 && (writes_done + 1) != FLAGS_num) {
+            batch.Put(key, gen.Generate());
+            batch_size++;
+          } else {
+            batch_size = 0;
+            db->Write(write_options_, &batch);
+            batch.Clear();
+          }
         } else {
-          s = db->Put(write_options_, key, gen.Generate());
-        }
-        if (!s.ok()) {
-          fprintf(stderr, "put error: %s\n", s.ToString().c_str());
-          ErrorExit();
+          if (user_timestamp_size_ > 0) {
+            Slice ts = mock_app_clock_->Allocate(ts_guard.get());
+            s = db->Put(write_options_, key, ts, gen.Generate());
+          } else {
+            s = db->Put(write_options_, key, gen.Generate());
+          }
+          if (!s.ok()) {
+            fprintf(stderr, "put error: %s\n", s.ToString().c_str());
+            ErrorExit();
+          }
         }
         put_weight--;
         writes_done++;
